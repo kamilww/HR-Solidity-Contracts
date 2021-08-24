a3d38bac1c52db2f24850e1a13880ea4199c6333
Kamil Wojnowski <kamilwojn@gmail.com>
2021-08-24 09:45:04 -0400

    completed contracts

--- DeferredEquityPlan.sol
+++ DeferredEquityPlan.sol
@@ -7,13 +7,15 @@ contract DeferredEquityPlan {
     address payable employee; // bob
     bool active = true; // this employee is active at the start of the contract
 
-    // @TODO: Set the total shares and annual distribution
-    // Your code here!
-
+    // Sets the total shares and annual distribution
+    uint total_shares = 1000;
+    uint annual_distribution = 250;
+    
+    // test variable uint fakenow = now;
     uint start_time = now; // permanently store the time this contract was initialized
 
-    // @TODO: Set the `unlock_time` to be 365 days from now
-    // Your code here!
+    // Sets the `unlock_time` to be 365 days from now
+    uint unlock_time = now + 365 days;
 
     uint public distributed_shares; // starts at 0
 
@@ -21,22 +23,27 @@ contract DeferredEquityPlan {
         human_resources = msg.sender;
         employee = _employee;
     }
+    
+    //function was used for testing purposes
+    //function fastforward() public {
+        //fakenow += 365 days;
+    //}
 
     function distribute() public {
         require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
         require(active == true, "Contract not active.");
 
-        // @TODO: Add "require" statements to enforce that:
+        // "require" statements to enforce that:
         // 1: `unlock_time` is less than or equal to `now`
+        require(unlock_time <= now, "Employee shares could only be distributed 365 days after last distribution.");
         // 2: `distributed_shares` is less than the `total_shares`
-        // Your code here!
+        require(distributed_shares < total_shares, "All 1000 shares have already been distributed.");
 
-        // @TODO: Add 365 days to the `unlock_time`
-        // Your code here!
+        // Add 365 days to the `unlock_time`
+        unlock_time +=  365 days;
 
-        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
-        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
-        // Your code here!
+        // Calculates the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
+        distributed_shares = (now - start_time) / 365 days * annual_distribution;
 
         // double check in case the employee does not cash out until after 5+ years
         if (distributed_shares > 1000) {
@@ -49,7 +56,7 @@ contract DeferredEquityPlan {
         require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
         active = false;
     }
-
+    
     // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
     function() external payable {
         revert("Do not send Ether to this contract!");
--- TieredProfitSplitter.sol
+++ TieredProfitSplitter.sol
@@ -5,7 +5,8 @@ contract TieredProfitSplitter {
     address payable employee_one; // ceo
     address payable employee_two; // cto
     address payable employee_three; // bob
-
+    
+    // contructor to set employee payable addresses
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
         employee_two = _two;
@@ -16,19 +17,29 @@ contract TieredProfitSplitter {
     function balance() public view returns(uint) {
         return address(this).balance;
     }
-
+    
+    // Deposit function sends tiered profit splitting to each employee address in contract
     function deposit() public payable {
         uint points = msg.value / 100; // Calculates rudimentary percentage by dividing msg.value into 100 units
         uint total;
         uint amount;
 
-        // @TODO: Calculate and transfer the distribution percentage
-        // Step 1: Set amount to equal `points` * the number of percentage points for this employee
-        // Step 2: Add the `amount` to `total` to keep a running total
-        // Step 3: Transfer the `amount` to the employee
+        // Calculates and transfers the distribution percentage for employee one
+        // Sets amount to equal `points` * the number of percentage points for this employee
+        amount = points * 60;
+        // Adds the `amount` to `total` to keep a running total
+        total += amount;
+        // Transfers the `amount` to the employee
+        employee_one.transfer(amount);
 
-        // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
-        // Your code here!
+        // repeat
+        amount = points * 25;
+        total += amount;
+        employee_two.transfer(amount);
+        
+        amount = points * 15;
+        total += amount;
+        employee_three.transfer(amount);
 
         employee_one.transfer(msg.value - total); // ceo gets the remaining wei
     }
